8fe7d4d9ae0b59708816d1c25bba2e97b1eabfe3
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-02-01 19:03:19 +0500

    📝 Written admin.py for User model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'role', 'phone_number', 'is_staff')
+    list_filter = ('role',)
+    search_fields = ('full_name',)
