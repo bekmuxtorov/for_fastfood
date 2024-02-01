49b2508ba0a8c4c5be4837b9656290b598c69d68
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-02-01 19:53:04 +0500

    📝 Written admin.py for order model

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Order
 
-# Register your models here.
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('get_user_full_name', 'latitude',
+                    'longitude', 'created_at', 'delivery_time')
+
+    search_fields = ('user__full_name',)
+
+    def get_user_full_name(self, obj):
+        return obj.user.full_name
+    get_user_full_name.short_description = 'User full_name'
