02cb281a113d68ad1ddd81e97368c1f041db06b5
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-02-01 21:58:48 +0500

    ➕ Added CRUD views

--- products/views.py
+++ products/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .import serializers
+from .import models
+
+
+# Meal Create api view
+class MealCreateAPIView(generics.CreateAPIView):
+    queryset = models.Meal.objects.all()
+    serializer_class = serializers.MealSerializer
+
+
+# Meal List api view
+class MealListAPIView(generics.ListAPIView):
+    queryset = models.Meal.objects.all()
+    serializer_class = serializers.MealSerializer
+
+
+# Meal Detail api view
+class MealGenericAPIView(generics.RetrieveAPIView):
+    queryset = models.Meal.objects.all()
+    serializer_class = serializers.MealSerializer
+
+
+# Meal Update api view
+class MealUpdateAPIView(generics.UpdateAPIView):
+    queryset = models.Meal.objects.all()
+    serializer_class = serializers.MealSerializer
+
+
+# Meal Delete api view
+class MealDeleteAPIView(generics.DestroyAPIView):
+    queryset = models.Meal.objects.all()
+    serializer_class = serializers.MealSerializer
