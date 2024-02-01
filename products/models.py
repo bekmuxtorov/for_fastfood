d9cc714cc4b8beadf2e6bb0aadf1138ea5aa8855
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-02-01 19:19:08 +0500

    ➕ Added Meal model in the products app

--- products/models.py
+++ products/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Meal(models.Model):
+    name = models.CharField(
+        verbose_name='Ovqat nomi',
+        max_length=100
+    )
+    cost = models.DecimalField(
+        verbose_name='Ovqat narxi(UZS):',
+        max_digits=6,
+        decimal_places=2
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
