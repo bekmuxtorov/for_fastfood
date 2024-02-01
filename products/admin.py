145b2622c6321b5da45345ae2edbeecd01481d9f
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-02-01 19:19:29 +0500

    📝 Written admin.py for Meal model

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Meal
 
-# Register your models here.
+
+@admin.register(Meal)
+class MealAdmin(admin.ModelAdmin):
+    list_display = ('name', 'cost', 'created_at')
+    search_fields = ('name',)
