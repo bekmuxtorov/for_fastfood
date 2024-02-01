76e9e3342047600d8ddf20888080acd74656f049
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-02-01 20:30:25 +0500

    📝 Added UserListAPIView

--- users/views.py
+++ users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .serializers import UserSerializer
+from .models import User
+
+
+class UserListAPIView(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
