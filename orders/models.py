f8fa6f11de713ca1f51b09298671f513f873294f
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-02-01 19:50:27 +0500

    ➕ Added Order model in orders app

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from users.models import User
+from products.models import Meal
 
-# Create your models here.
+
+class Order(models.Model):
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        related_name='orders',
+        verbose_name='Mijoz',
+    )
+    meals = models.ManyToManyField(
+        to=Meal,
+        related_name="orders",
+    )
+    latitude = models.FloatField(
+        verbose_name='Joylashuv latitude'
+    )
+    longitude = models.FloatField(
+        verbose_name='Joylashuv longitude'
+    )
+    created_at = models.DateTimeField(
+        verbose_name="Qo'shilgan vaqt",
+        auto_now_add=True
+    )
+    delivery_time = models.DateTimeField(
+        blank=True,
+        null=True,
+        verbose_name="Tahminiy yetkazib berish vaqti"
+    )
+
+    def __str__(self):
+        return ' | '.join(["Order", str(self.id), self.user.phone_number])
